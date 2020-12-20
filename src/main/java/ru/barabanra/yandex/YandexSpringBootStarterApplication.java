205a3a43ad3ae5d7e1afa480f863ebb5f5f91e33
barabanra <r.baraban.alex@gmail.com>
2020-12-20 11:54:14 +0400

    refactoring tests, starter application

--- src/main/java/ru/barabanra/yandex/YandexSpringBootStarterApplication.java
+++ src/main/java/ru/barabanra/yandex/YandexSpringBootStarterApplication.java
@@ -1,16 +1,6 @@
 package ru.barabanra.yandex;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class YandexSpringBootStarterApplication {
     public static void main(String[] args) {
-        SpringApplication.run(YandexSpringBootStarterApplication.class, args);
     }
 }
-
-//public class YandexSpringBootStarterApplication {
-//    public static void main(String[] args) {
-//    }
-//}
